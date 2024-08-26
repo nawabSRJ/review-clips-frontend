0a4c25ad43b268311c0f562d1796176f282d5e8e
Srajan Saxena <srajan.saxena7@gmail.com>
2024-08-26 17:05:09 +0530

    validation added , loading added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -11,6 +11,7 @@ export default function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
 
@@ -52,11 +53,12 @@ export default function Register() {
     if (!validateInputs()) {
       return; // Exit if validation fails
     }
-
+    setLoading(true); // when the request starts -> set to true
     axios.post('https://review-clips-backend.onrender.com/register', { name, email, password })
       .then(result => {
         console.log(result)
         navigate('/login')
+
       })
 
       .catch(err => console.log(err));
@@ -64,17 +66,24 @@ export default function Register() {
 
   return (
     <div className='flex sm:flex-row flex-col min-h-screen justify-center items-center'>
-      <div className='registerBox w-[500px] h-[400px] bg-blue-600 text-center mx-auto'>
-        <h2 className='text-3xl text-white px-5 py-3 font-cambria'><b>Sign-Up</b></h2>
-        <form onSubmit={handleSubmit}>
-          <input type='text' className='px-4 py-2 my-3 ' placeholder='Enter Name...' onChange={(e) => setName(e.target.value)} /> <br />
-          <input type='email' className='px-4 py-2 my-3 ' placeholder='Enter email id...' onChange={(e) => setEmail(e.target.value)} /> <br />
-          <input type='password' className='px-4 py-2 my-3 ' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} />
-          <button type='submit' className='w-[80%] mt-3 text-2xl text-center bg-white text-blue-600 py-3'>SignUp</button>
+      {
+        loading
+          ?
+          <img src='https://i.gifer.com/ZKZg.gif' alt='loading gif' />
+          :
+          <div className='registerBox w-[500px] h-[400px] bg-blue-600 text-center mx-auto'>
+            <h2 className='text-3xl text-white px-5 py-3 font-cambria'><b>Sign-Up</b></h2>
+            <form onSubmit={handleSubmit}>
+              <input type='text' className='px-4 py-2 my-3 ' placeholder='Enter Name...' onChange={(e) => setName(e.target.value)} /> <br />
+              <input type='email' className='px-4 py-2 my-3 ' placeholder='Enter email id...' onChange={(e) => setEmail(e.target.value)} /> <br />
+              <input type='password' className='px-4 py-2 my-3 ' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} />
+              <button type='submit' className='w-[80%] mt-3 text-2xl text-center bg-white text-blue-600 py-3'>SignUp</button>
 
-          <p className='text-blue-600 py-1 rounded-full w-[70%] mx-auto bg-white mt-5'>Already have an account? <Link to={'/login'}><span className='text-underline text-black'>Sign-in</span></Link></p>
-        </form>
-      </div>
+              <p className='text-blue-600 py-1 rounded-full w-[70%] mx-auto bg-white mt-5'>Already have an account? <Link to={'/login'}><span className='text-underline text-black'>Sign-in</span></Link></p>
+            </form>
+          </div>
+      }
+
       <NotificationContainer />
     </div>
   );
