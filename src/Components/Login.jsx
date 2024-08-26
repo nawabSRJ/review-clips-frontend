abced5ba3adb7e0b08104e319e4b06936108c685
Srajan Saxena <srajan.saxena7@gmail.com>
2024-08-26 15:40:32 +0530

    loading added , build done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -13,10 +13,10 @@ export default function Login() {
     const handleSubmit = (e) => {
         e.preventDefault();
         setLoading(true); // when the request starts -> set to true
-        axios.post('https://review-clips-backend.onrender.com/login', {email, password })
+        axios.post('https://review-clips-backend.onrender.com/login', { email, password })
             .then(result => {
                 console.log(result)
-                if(result.data.message === "Success"){
+                if (result.data.message === "Success") {
                     navigate('/dashboard', { state: { user: result.data.user } });
                     setLoading(false);
                 }
@@ -25,18 +25,25 @@ export default function Login() {
                 console.log(err)
                 setLoading(true);
             });
-            
+
     };
     return (
         <div className='flex sm:flex-row flex-col min-h-screen justify-center items-center'>
             <div className='LoginBox w-[500px] h-[300px] bg-blue-600 text-center mx-auto'>
                 <h2 className='text-3xl text-white px-5 py-3 font-cambria'><b>Login</b></h2>
-                <form onSubmit={handleSubmit}>
+                {
+                    loading
 
-                    <input type='email' className='px-4 py-2 my-3 ' placeholder='Enter email id...' onChange={(e) => setEmail(e.target.value)} /> <br />
-                    <input type='password' className='px-4 py-2 my-3 ' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} />
-                    <button type='submit' className='w-[80%] mt-3 text-2xl text-center bg-white text-blue-600 py-3'>Login</button>
-                </form>
+                        ?
+                        <img src='https://i.gifer.com/ZKZg.gif' />
+                        :
+                        <form onSubmit={handleSubmit}>
+                            <input type='email' className='px-4 py-2 my-3 ' placeholder='Enter email id...' onChange={(e) => setEmail(e.target.value)} /> <br />
+                            <input type='password' className='px-4 py-2 my-3 ' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} />
+                            <button type='submit' className='w-[80%] mt-3 text-2xl text-center bg-white text-blue-600 py-3'>Login</button>
+                        </form>
+                }
+
             </div>
         </div>
     )
