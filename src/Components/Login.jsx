a3d144bfd07ef845dc2fc17a6726f9d0d8d33ca1
Srajan Saxena <srajan.saxena7@gmail.com>
2024-09-05 11:26:44 +0530

    minor change

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
             })
             .catch(err => {
                 console.log(err)
-                setLoading(true);
+                setLoading(false);
             });
 
     };
